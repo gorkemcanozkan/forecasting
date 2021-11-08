e071f773e3c65532436e780ef9c368230e96e2b0
Görkem Can Özkan <71969715+gorkemcanozkan@users.noreply.github.com>
2021-11-08 13:59:01 +0300

    Update main.py

--- main.py
+++ main.py
@@ -15,19 +15,19 @@ elif select=="Ortalama":
     select_="yhat"
 elif select=="Kötümser":
     select_="yhat_lower"
-
-col_list = df.columns[1:]
-ff = []
-for item in col_list:
-    df = df.rename(columns={item: "y"})
-    df1 = df[['ds', 'y']]
-    m = Prophet()
-    m.fit(df1)
-    future = m.make_future_dataframe(periods=donem, freq='M')
-    forecast = m.predict(future)
-    ff.append(forecast[select_].tail(donem))
-    df.drop(columns=["y"], inplace=True)
-x=pd.DataFrame(ff).T
-x.columns=col_list
-x[x<0]=0
-st.write(x)
+if st.button("Tahminle"):
+    col_list = df.columns[1:]
+    ff = []
+    for item in col_list:
+        df = df.rename(columns={item: "y"})
+        df1 = df[['ds', 'y']]
+        m = Prophet()
+        m.fit(df1)
+        future = m.make_future_dataframe(periods=donem, freq='M')
+        forecast = m.predict(future)
+        ff.append(forecast[select_].tail(donem))
+        df.drop(columns=["y"], inplace=True)
+    x=pd.DataFrame(ff).T
+    x.columns=col_list
+    x[x<0]=0
+    st.write(x)
