3c708748b2dc8e8cf2f641794e6d433b854e8a1a
Görkem Can Özkan <71969715+gorkemcanozkan@users.noreply.github.com>
2021-11-08 12:43:37 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,32 @@
+import streamlit as st
+import pandas as pd
+import numpy as np
+import openpyxl
+from fbprophet import Prophet
+
+st.title("Forecasting")
+df=pd.read_excel(st.file_uploader("Dosyanızı Import Ediniz"))
+k_adi=st.sidebar.text_input("Kullanıcı Adı")
+sifre=st.sidebar.text_input("Şifre")
+donem=st.sidebar.text_input("Kaç Dönem Tahmin Etmek İstiyorsunuz?")
+select=st.sidebar.radio("İyimser - Orta - Kötümser Tahmin", ("İyimser","Ortalama","Kötümser"))
+if select=="İyimser":
+    select_="yhat_upper"
+elif select=="Ortalama":
+    select_="yhat"
+elif select=="Kötümser":
+    select_="yhat_lower"
+
+if k_adi=="gizemsenol" and sifre=="publicis.1234@2021":
+    col_list = df.columns[1:]
+    ff = []
+    for item in col_list:
+        df = df.rename(columns={item: "y"})
+        df1 = df[['ds', 'y']]
+        m = Prophet()
+        m.fit(df1)
+        future = m.make_future_dataframe(periods=donem, freq='M')
+        forecast = m.predict(future)
+        ff.append(forecast[select_].tail(1).values[0])
+        df.drop(columns=["y"], inplace=True)
+    pd.DataFrame(ff3).T
